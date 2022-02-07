b25edbdfad99253d6d06ebab1871c63f135a9dc4
Deresh08 <36786938+Deresh08@users.noreply.github.com>
2022-02-07 13:28:30 +0100

    Update WalletService.java
    added logic to generate wallet number, and user password. new method creates new player if does not exist

--- src/main/java/com/amazinggaming/wallet/service/WalletService.java
+++ src/main/java/com/amazinggaming/wallet/service/WalletService.java
@@ -5,13 +5,19 @@ package com.amazinggaming.wallet.service;
 
 
 import java.math.BigDecimal;
+import java.security.SecureRandom;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.amazinggaming.wallet.domain.Player;
+import com.amazinggaming.wallet.domain.Profile;
 import com.amazinggaming.wallet.domain.TransactionType;
 import com.amazinggaming.wallet.domain.Wallet;
+import com.amazinggaming.wallet.domain.WalletStatus;
 import com.amazinggaming.wallet.domain.WalletType;
 import com.amazinggaming.wallet.dto.BaseDto;
 import com.amazinggaming.wallet.dto.WalletDto;
@@ -42,6 +48,9 @@ public class WalletService {
 	@Autowired
 	private TransactionService transactionService;
 	
+	
+	private static final int WALLET_NO_SIZE = 15;
+	
 	/**
 	 * 
 	 * This method allows players to deposit funds
@@ -51,13 +60,22 @@ public class WalletService {
 	 * @throws ApplicationException
 	 */
 	public WalletDto deposit(WalletDto deposit) throws ApplicationException {
+		boolean returnPassword = false;
 		WalletDto response = new WalletDto();
 		TransactionType transactionType = transactionTypeRepository.findByCode(deposit.getTransactionCode());
 		Player player = playerRepository.findByUsername(deposit.getUsername());
+		if(player == null) {
+			createPlayer(deposit.getUsername());
+			returnPassword = true;
+		}
+		player = playerRepository.findByUsername(deposit.getUsername());
 		Wallet playersWallet = walletRepository.findByUserAndWalletType(player, WalletType.PLAYER_WALLET);
 		Wallet playersBonusWallet = walletRepository.findByUserAndWalletType(player, WalletType.PLAYER_BONUS_WALLET);
 		BaseDto transactionResult = transactionService.createTransactionAndApplyCharges(deposit.getAmount(), playersWallet, transactionType,
 				deposit.getTransactionId(), player);
+		if(returnPassword) {
+			response.setPassword(player.getPassword());
+		}
 		response.setTransactionId(transactionResult.getTransactionId());
 		response.setExternalTransactionId(transactionResult.getExternalTransactionId());
 		response.setCashBalance(playersWallet.getBalance());
@@ -101,5 +119,58 @@ public class WalletService {
 		walletDto.setBonusBalance(playersBonusWallet.getBalance());
 		return walletDto;
 	}
-
+	
+	
+	
+	private void createPlayer(String username) {
+		Player player = new Player();
+		player.setUsername(username);
+		player.setPassword(generateRandomPassword(WALLET_NO_SIZE));
+		player.setProfile(Profile.PLAYER);
+		playerRepository.save(player);
+		createPlayerWallets(player);
+	}
+	
+	private void createPlayerWallets(Player player) {
+		Wallet cashWallet = new Wallet();
+		Wallet bonusWallet = new Wallet();
+		cashWallet.setBalance(BigDecimal.ZERO);
+		cashWallet.setWalletNumber(generateRandomWalletNumber(WALLET_NO_SIZE));
+		cashWallet.setWalletStatus(WalletStatus.ACTIVE);
+		cashWallet.setUser(player);
+		cashWallet.setWalletType(WalletType.PLAYER_WALLET);
+		bonusWallet.setBalance(BigDecimal.ZERO);
+		bonusWallet.setWalletNumber(generateRandomWalletNumber(WALLET_NO_SIZE));
+		bonusWallet.setWalletStatus(WalletStatus.ACTIVE);
+		bonusWallet.setUser(player);
+		bonusWallet.setWalletType(WalletType.PLAYER_BONUS_WALLET);
+		walletRepository.save(cashWallet);
+		walletRepository.save(bonusWallet);
+	}
+	
+	
+	private String generateRandomWalletNumber(int length) {
+	    Random random = new Random();
+	    char[] digits = new char[length];
+	    digits[0] = (char) (random.nextInt(9) + '1');
+	    for (int i = 1; i < length; i++) {
+	        digits[i] = (char) (random.nextInt(10) + '0');
+	    }
+	    return new String(digits);
+	}
+	
+	public static String generateRandomPassword(int len)
+    {
+        // ASCII range – alphanumeric (0-9, a-z, A-Z)
+        final String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+ 
+        SecureRandom random = new SecureRandom();
+ 
+        // each iteration of the loop randomly chooses a character from the given
+        // ASCII range and appends it to the `StringBuilder` instance
+        return IntStream.range(0, len)
+                .map(i -> random.nextInt(chars.length()))
+                .mapToObj(randomIndex -> String.valueOf(chars.charAt(randomIndex)))
+                .collect(Collectors.joining());
+    }
 }
